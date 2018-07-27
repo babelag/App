60def28d72a0ad813b1155e5e5ec8b5a2b79307f
babelag <hub.lewandowski@o2.pl>
2018-07-27 14:22:38 +0200

    0.19

--- starter_code/tests/system/test_item.py
+++ starter_code/tests/system/test_item.py
@@ -5,34 +5,94 @@ from tests.base_test import BaseTest
 import json
 
 class ItemTest(BaseTest):
-    def test_get_item_no_auth(self):
+    def setUp(self):
+        super(ItemTest, self).setUp()
         with self.app() as client:
             with self.app_context():
+                UserModel('test', '1234').save_to_db()
+                author_request = client.post('/auth',
+                                             data = json.dumps({'username' : 'test', 'password' : '1234'}),
+                                             headers = {'Content-Type' : 'application/json'})
+                self.auth_header = "JWT {}".format(json.loads(author_request.data)['access_token'])
+
+    def test_get_item_no_auth(self):
+        with self.app() as client:
                 response = client.get('/item/test')
 
                 self.assertEqual(response.status_code, 401)
 
-
     def test_get_item_not_found(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                response = client.get('/item/test', headers = {'Authorization' : self.auth_header})
+                self.assertEqual(response.status_code, 404)
 
     def test_get_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                StoreModel('test').save_to_db()
+                ItemModel('test', 11.99, 1).save_to_db()
+                response = client.get('/item/test', headers = {'Authorization' : self.auth_header})
+                self.assertEqual(response.status_code, 200)
 
     def test_delete_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                StoreModel('test').save_to_db()
+                ItemModel('test', 11.99, 1).save_to_db()
+
+                response = client.delete('/item/test')
+                self.assertEqual(response.status_code, 200)
+                self.assertDictEqual({'message' : 'Item deleted'}, json.loads(response.data))
 
     def test_create_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                StoreModel('test').save_to_db()
+
+                response = client.post('/item/test', data = {'price' : 11.99, 'store_id' : 1})
+                self.assertEqual(response.status_code, 201)
+                self.assertDictEqual({'name' : 'test', 'price' : 11.99}, json.loads(response.data))
 
     def test_create_duplicate_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                StoreModel('test').save_to_db()
+                ItemModel('test', 11.99, 1).save_to_db()
+                response = client.post('/item/test', data={'price': 11.99, 'store_id': 1})
+                self.assertEqual(response.status_code, 400)
+                self.assertDictEqual({'message': 'An item with name \'test\' already exists.'}, json.loads(response.data))
 
     def test_put_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                StoreModel('test').save_to_db()
+                response = client.put('/item/test', data = {'price' : 11.99, 'store_id' : 1})
+
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(ItemModel.find_by_name('test').price, 11.99)
+                self.assertDictEqual({'name' : 'test', 'price' : 11.99}, json.loads(response.data))
 
     def test_put_update_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                StoreModel('test').save_to_db()
+                ItemModel('test', 5.99, 1).save_to_db()
+
+                self.assertEqual(ItemModel.find_by_name('test').price, 5.99)
+
+                response = client.put('/item/test', data={'price': 11.99, 'store_id': 1})
+
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(ItemModel.find_by_name('test').price, 11.99)
+                self.assertDictEqual({'name': 'test', 'price': 11.99}, json.loads(response.data))
 
     def test_item_list(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                StoreModel('test').save_to_db()
+                ItemModel('test', 11.99, 1).save_to_db()
+
+                response = client.get('/items')
+
+                self.assertEqual({'items' : [{'name' : 'test', 'price' : 11.99}]}, json.loads(response.data))
